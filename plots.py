23fe48ca10568ea68a416694245de21c57290fa6
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-07-03 17:22:28 +0100

    Plots changed to put proxy on the x-axis.

--- plots.py
+++ plots.py
@@ -4,12 +4,12 @@ import plotutils.plotutils as pu
 import triangle as tri
 
 def _plot_data(logpost, *args, **kwargs):
-    pp.errorbar(np.exp(logpost.obs_masses), np.exp(logpost.obs_proxies), *args,
+    pp.errorbar(np.exp(logpost.obs_proxies), np.exp(logpost.obs_masses), *args,
                 fmt='.', color='k',
-                xerr=(-np.exp(logpost.obs_masses)*(np.expm1(-logpost.obs_dmasses)),
-                      np.exp(logpost.obs_masses)*np.expm1(logpost.obs_dmasses)),
-                yerr=(-np.exp(logpost.obs_proxies)*(np.expm1(-logpost.obs_dproxies)),
+                xerr=(-np.exp(logpost.obs_proxies)*(np.expm1(-logpost.obs_dproxies)),
                       np.exp(logpost.obs_proxies)*np.expm1(logpost.obs_dproxies)),
+                yerr=(-np.exp(logpost.obs_masses)*(np.expm1(-logpost.obs_dmasses)),
+                      np.exp(logpost.obs_masses)*np.expm1(logpost.obs_dmasses)),
                 **kwargs)
     pp.xscale('log')
     pp.yscale('log')
@@ -30,7 +30,7 @@ def plot_fit_data(logpost, flatchain):
 
     _plot_data(logpost)
 
-    pp.plot(np.exp(mean_masses), np.exp(proxies), '-b')
+    pp.plot(np.exp(proxies), np.exp(mean_masses), '-b')
 
     for i in range(10):
         ind = np.random.randint(flatchain.shape[0])
@@ -41,10 +41,10 @@ def plot_fit_data(logpost, flatchain):
             ms.append(logpost.mass_estimate_mean_variance(p, px, 0.0)[0])
         ms = np.array(ms)
 
-        pp.plot(np.exp(ms), np.exp(proxies), '-b', alpha=0.25)
+        pp.plot(np.exp(proxies), np.exp(ms), '-b', alpha=0.25)
 
-    pp.xlabel(r'$M$')
-    pp.ylabel(r'$M_\mathrm{proxy}$')
+    pp.ylabel(r'$M$')
+    pp.xlabel(r'$M_\mathrm{proxy}$')
 
 def plot_corner(logpost, chain):
     tri.corner(chain.reshape((-1, chain.shape[2])), labels=logpost.pnames, quantiles=[0.05, 0.95])
