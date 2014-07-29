12bde08241afe253359c70be5b209ae5f40a8262
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-07-29 17:09:17 +0100

    Changes to plots.py to match latest choices for the paper.

--- plots.py
+++ plots.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as pp
+import os.path as op
 import plotutils.plotutils as pu
+import scipy.stats as ss
 import triangle as tri
 
 def setup():
@@ -15,6 +17,9 @@ def setup():
               'text.usetex': True,
               'figure.figsize': [width,width]}
     mpl.rcParams.update(params)
+
+def axes():
+    pp.axes([0.16, 0.16, 0.95-0.16, 0.95-0.16])
 
 def plot_corner(logpost, chain):
     tri.corner(chain.reshape((-1, chain.shape[2])), labels=logpost.pnames, quantiles=[0.05, 0.95])
@@ -34,80 +39,87 @@ def logerrorbar(xs, ys, xerr=None, yerr=None, *args, **kwargs):
     pp.xscale('log')
     pp.yscale('log')
 
-def plot_est_masses(logpost, chain, obs_proxies, obs_dproxies, obs_masses, obs_dmasses, ptrue, mtrue, alpha, b):
-    pp.axes([0.16, 0.16, 0.95-0.16, 0.95-0.16])
-
-    masses = np.zeros(obs_proxies.shape[0])
-    dmasses = np.zeros(obs_dproxies.shape[0])
-    proxies = np.zeros(obs_proxies.shape[0])
-    dproxies = np.zeros(obs_dproxies.shape[0])
+def plot_mass_estimates(logpost, chain, obs_proxies, obs_dproxies, mtrues, outdir=None):
+    if outdir is not None:
+        setup()
+        axes()
 
     flatchain = chain.reshape((-1, chain.shape[2]))
 
-    for par in flatchain:
-        for i, (p, dp) in enumerate(zip(obs_proxies, obs_dproxies)):
-            mu, v = logpost.mass_proxy_estimate(par, p, dp)
-            masses[i] += mu[0]
-            proxies[i] += mu[1]
-            dmasses[i] += v[0]
-            dproxies[i] += v[1]
+    mests = []
+    mvars = []
+    for pobs, dp, m in zip(obs_proxies, obs_dproxies, mtrues):
+        ms = []
+        vs = []
+        for p in flatchain:
+            (mest, _), (vest, _) = logpost.mass_proxy_estimate(p, pobs, dp)
+            ms.append(mest-m)
+            vs.append(vest)
+        mests.append(np.array(ms))
+        mvars.append(np.array(vs))
+    mests = np.array(mests)
+    mvars = np.array(mvars)
 
-    masses /= flatchain.shape[0]
-    dmasses /= flatchain.shape[0]
-    proxies /= flatchain.shape[0]
-    dproxies /= flatchain.shape[0]
+    xs = np.linspace(-4, 4, 500)
+    ys = []
+    for mest, mvar in zip(mests, mvars):
+        y = 0
+        for m, v in zip(mest, mvar):
+            y += ss.norm.pdf(xs, loc=m, scale=np.sqrt(v))
+        y /= mest.shape[0]
+        ys.append(y)
+    ys = np.array(ys)
 
-    dmasses = np.sqrt(dmasses)
-    dproxies = np.sqrt(dproxies)
+    for y in ys:
+        pp.plot(np.exp(xs), y)
+    pp.xscale('log')
+    pp.xlabel(r'$M_{500}/M_{500}^{true}$')
+    pp.ylabel(r'$p\left(\ln \, M_{500}/M_{500}^{true}\right)$')
 
-    logerrorbar(obs_proxies, obs_masses, xerr=obs_dproxies, yerr=obs_dmasses, color='k', fmt='.')
-    logerrorbar(proxies, masses, xerr=dproxies, yerr=dmasses, color='b', fmt='.')
+    if outdir is not None:
+        pp.savefig(op.join(outdir, 'pmest.pdf'))
+        with open(op.join(outdir, 'pmest.dat'), 'w') as out:
+            out.write('# ln(M500/M500true)\tp1\tp2\t...\n')
+            np.savetxt(out, np.column_stack((xs, ys.T)))
 
-    xs = np.linspace(min(np.min(proxies), np.min(obs_proxies)) - 0.3,
-                     max(np.max(proxies), np.max(obs_proxies)) + 0.3,
-                     100)
-    ys = alpha*xs + b
+def plot_mass_corrections(logpost, chain, obs_proxies, obs_dproxies, proxies, masses, alpha, b, outdir=None):
+    if outdir is not None:
+        setup()
+        axes()
 
-    pp.plot(np.exp(xs), np.exp(ys), '-k')
-    pp.plot(np.exp(ptrue), np.exp(masses), '*k')
+    flatchain = chain.reshape((-1, chain.shape[2]))
 
-    pp.xscale('log', nonposx='clip')
-    pp.yscale('log', nonposy='clip')
+    # Plot the "truth" line
+    pp.plot(np.exp(np.sort(proxies)), np.exp(alpha*np.sort(proxies) + b), '-k')
 
+    # Compute the mass estimate and variance
+    pred_ms = []
+    pred_dms = []
+    for pobs, dp in zip(obs_proxies, obs_dproxies):
+        ms = []
+        vs = []
+        for p in flatchain:
+            (m, _), (dm, _) = logpost.mass_proxy_estimate(p, pobs, dp)
+            ms.append(m)
+            vs.append(dm)
+        pred_ms.append(np.mean(ms))
+        pred_dms.append(np.sqrt(np.mean(vs)))
+    pred_ms = np.array(pred_ms)
+    pred_dms = np.array(pred_dms)
+
+    # Plot truth
+    pp.plot(np.exp(proxies), np.exp(masses), '*k')
+
+    # Plot the observations and predictions
+    logerrorbar(obs_proxies, pred_ms, xerr=obs_dproxies, yerr=pred_dms, color='k', fmt='.')
+
+    pp.xscale('log')
+    pp.yscale('log')
     pp.xlabel(r'$X$')
-    pp.ylabel(r'$M$')
+    pp.ylabel(r'$M_{500}$')
 
-def plot_fit(logpost, chain):
-    pp.axes([0.16, 0.16, 0.95-0.16, 0.95-0.16])
-
-    flatchain = chain.reshape((-1, 5))
-
-    logerrorbar(logpost.obs_proxies, logpost.obs_masses, xerr=logpost.obs_dproxies, yerr=logpost.obs_dmasses, color='k', fmt='.')
-    
-    alphas = []
-    mus = []
-    for p in flatchain:
-        alphas.append(logpost.alpha(p))
-        mus.append(p[0:2])
-    alphas = np.array(alphas)
-    mus = np.array(mus)
-
-    mean_alpha = np.mean(alphas)
-    mean_b = np.mean(mus[:,0] - alphas*mus[:,1])
-
-    xs = np.linspace(np.min(logpost.obs_proxies) - np.max(logpost.obs_dproxies), \
-                     np.max(logpost.obs_proxies) + np.max(logpost.obs_dproxies), \
-                     100)
-    ys = mean_alpha*xs + mean_b
-
-    pp.plot(np.exp(xs), np.exp(ys), '-k')
-
-    for i in range(10):
-        index = np.random.randint(alphas.shape[0])
-        al = alphas[index]
-        b = mus[index,0] - al*mus[index,1]
-        ys = al*xs + b
-        pp.plot(np.exp(xs), np.exp(ys), '-k', alpha=0.1)
-
-    pp.xlabel(r'$X$')
-    pp.ylabel(r'$M$')
+    if outdir is not None:
+        pp.savefig(op.join(outdir, 'mcorr.pdf'))
+        with open(op.join(outdir, 'mcorr.dat'), 'w') as out:
+            out.write('# ln(X)\tln(M)\tln(X_obs)\tdX_obs/X_obs\tln(M_pred)\tdM_pred/M_pred\n')
+            np.savetxt(out, np.column_stack((proxies, masses, obs_proxies, obs_dproxies, pred_ms, pred_dms)))
