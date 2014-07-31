7f4410ae1e3856807d27f3aa726e52c1cb3897f2
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-07-31 18:10:06 +0100

    Added easy calibration script

---
+++ calibrate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+import emcee
+import numpy as np
+import os.path as op
+import plotutils.runner as pr
+import posterior as pos
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data', metavar='FILE', required=True, help='observational data')
+    parser.add_argument('--outdir', metavar='DIR', default='.', help='output directory (default %(default)s)')
+
+    args = parser.parse_args()
+
+    # Data file should have 
+    #
+    # Mobs dM/M Xobs dX/X
+    # 
+    # format
+    data = np.loadtxt(args.data)
+    
+    logpost = pos.Posterior(np.log(data[:,0]), np.log(data[:,2]), data[:,1], data[:,3])
+    sampler = emcee.EnsembleSampler(128, logpost.nparams, logpost)
+    runner = pr.EnsembleSamplerRunner(sampler, logpost.pguess() + 1e-3*np.random.randn(128, logpost.nparams))
+    runner.run_to_neff(16, savedir=args.outdir)
